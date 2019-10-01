b3087f1b5dad58960624b9dce8d864c468d81eae
Rubens-Gaia <alvares.rubens@gmail.com>
2019-10-01 11:17:34 -0300

    Versão bonita

--- calculadora2.js
+++ calculadora2.js
@@ -5,7 +5,7 @@ var resultado;
 var visor;
 
 function teclaNumerica(numero) {
-        operando1 = operando1.concat(numero); 
+        
         document.getElementById("visor").value = numero;
 }
 
