a7cb7701b5a1f9b12f9aff64580570cc30be8190
Rubens-Gaia <alvares.rubens@gmail.com>
2019-10-02 11:57:47 -0300

    Bugada meio funcionando

--- calculadora2.js
+++ calculadora2.js
@@ -5,62 +5,60 @@ var visor;
 var resultado;
 
 function teclaNumerica(numero) {
-
     if (typeof visor == 'undefined') {
         document.getElementById("visor").value = '';
     }
-    document.getElementById("visor").value = document.getElementById("visor").value + numero;
+    visor = document.getElementById("visor").value = document.getElementById("visor").value + numero;
     visor = 1;
-
 }
 
 function teclaFuncao(operacao) {
-
     valor1 = document.getElementById("visor").value;
-    document.getElementById("visor").value = '';
-    delete visor;
+
+    if (typeof operador != 'undefined') {
+
+        operador = operacao;
+        document.getElementById("visor").value = '';
+
+    } else {
+        operador = operacao;
+        document.getElementById("visor").value = '';
+    }
 
     if (operacao == 'apagar') {
         document.getElementById("visor").value = '';
         delete visor;
+        delete operador;
         delete valor1;
         delete valor2;
         delete resultado;
-
     }
-    if (typeof operador != 'undefined' && operacao == 'resultado') {
+}
 
-        resultado = calcular(operador, valor1, valor2);
-        document.getElementById("visor").value = resultado;
-        delete operacao;
-        delete resultado;
-        return (0);
-    }
-
-    if (typeof resultado != 'undefined') {
-        resultado = calcular(operacao, valor1, valor2);
-        operador = operacao;
-        document.getElementById("visor").value = resultado;
-
-    } else {
-        valor2 = document.getElementById("visor").value;
-        operador = operacao;
-    }
+function resultado() {
+    valor2 = document.getElementById("visor").value;
+    resultado = calcular(operador, valor1, valor2);
+    document.getElementById("visor").value = resultado;
+    delete operador;
+    delete valor1;
+    delete valor2;
+    delete visor;
 }
 
 function calcular(operador, valor1, valor2) {
 
     switch (operador) {
-        case "somar":
-            resultado = valor1 + valor2;
-            alert("somar" + operador + valor1 + valor2);
-            return resultado;
-        case "subtrair":
-            return valor1 - valor2;
-        case "multiplicar":
-            return valor1 * valor2;
-        case "dividir":
-            return valor1 / valor2;
+        case "+":
+            final = parseInt(valor1) + parseInt(valor2);
+            return final;
+        case "-":
+            final = parseInt(valor1) - parseInt(valor2);
+            return final;
+        case "*":
+            final = parseInt(valor1) * parseInt(valor2);
+            return final;
+        case "/":
+            final = parseInt(valor1) / parseInt(valor2);
+            return final;
     }
-
 }
