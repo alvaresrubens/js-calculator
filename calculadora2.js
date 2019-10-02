d1c998b435105fd7039113751b91ffffd8709382
Rubens-Gaia <alvares.rubens@gmail.com>
2019-10-02 14:47:41 -0300

    Funcional e sem bugg

--- calculadora2.js
+++ calculadora2.js
@@ -2,45 +2,37 @@ var valor1;
 var valor2;
 var operador;
 var visor;
+var resultado;
 
 function teclaNumerica(numero) {
-    if (typeof visor == 'undefined') {
-        document.getElementById("visor").value = '';
-    }
+    /*
+        if (typeof visor == 'undefined') {
+            document.getElementById("visor").value = '';
+        }*/
     document.getElementById("visor").value = document.getElementById("visor").value + numero;
-    visor = 1;
+    //visor = 1;
 }
 
 function teclaFuncao(operacao) {
     valor1 = document.getElementById("visor").value;
-
-    if (operador == null) {
-
-        operador = operacao;
-        document.getElementById("visor").value = '';
-        alert("Cálculo: " + valor1 + operador + valor2)
-
-    } else {
-        //document.getElementById("visor").value = '';
-        alert("Operador já definido " + operador);
-    }
+    operador = operacao;
+    document.getElementById("visor").value = valor1 + operacao;
 
 }
 
 function limpar() {
-
     document.getElementById("visor").value = '';
-    operador = null;
-    valor1 = null;
-    valor2 = null;
-    delete visor;
-    alert("Limpou");
+    operador = "";
+    valor1 = "";
+    valor2 = "";
+    visor = "";
 }
 
-function resultado() {
-    alert("Entrou em resultado");
+function igual() {
+
+
+
     valor2 = document.getElementById("visor").value;
-    alert("Cálculo: " + valor1 + " " + operador + " " + valor2);
     resultado = calcular(operador, valor1, valor2);
     document.getElementById("visor").value = resultado;
 
